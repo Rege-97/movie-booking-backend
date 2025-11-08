8a133269ddc01c94369c4afcc274fc281eb9df72
Rege-97 <cogus1027@gmail.com>
2025-11-08 18:24:46 +0900

    :sparkles: feat(auth): 로그인 API 추가

--- src/main/java/com/cinema/moviebooking/common/response/ApiResponse.java
+++ src/main/java/com/cinema/moviebooking/common/response/ApiResponse.java
@@ -13,8 +13,8 @@ public class ApiResponse<T> {
     private String message;
     private T data;
 
-    public static <T> ApiResponse<T> success(T data) {
-        return new ApiResponse<>("SUCCESS", null, data);
+    public static <T> ApiResponse<T> success(T data, String message) {
+        return new ApiResponse<>("SUCCESS", message, data);
     }
 
     public static <T> ApiResponse<T> created(T data, String message) {
--- src/main/java/com/cinema/moviebooking/controller/AuthController.java
+++ src/main/java/com/cinema/moviebooking/controller/AuthController.java
@@ -1,6 +1,8 @@
 package com.cinema.moviebooking.controller;
 
 import com.cinema.moviebooking.common.response.ApiResponse;
+import com.cinema.moviebooking.dto.auth.LoginRequest;
+import com.cinema.moviebooking.dto.auth.LoginResponse;
 import com.cinema.moviebooking.dto.auth.SignUpRequest;
 import com.cinema.moviebooking.dto.auth.SignUpResponse;
 import com.cinema.moviebooking.service.AuthService;
@@ -25,5 +27,12 @@ public class AuthController {
         SignUpResponse signUpResponse = authService.signUp(req);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(ApiResponse.created(signUpResponse, "회원가입이 완료되었습니다."));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@Valid @RequestBody LoginRequest req) {
+        LoginResponse loginResponse = authService.login(req);
+        return ResponseEntity.ok()
+                .body(ApiResponse.success(loginResponse, "로그인 되었습니다."));
     }
 }
