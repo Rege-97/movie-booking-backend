18191d3b15b5e67d1bf8c3fa03e4e44eda45037f
Rege-97 <cogus1027@gmail.com>
2025-11-09 18:17:53 +0900

    :sparkles: feat(theater): 엔티티 필드 추가

--- src/main/java/com/cinema/moviebooking/entity/Theater.java
+++ src/main/java/com/cinema/moviebooking/entity/Theater.java
@@ -19,13 +19,25 @@ public class Theater extends BaseTimeEntity {
     private String name;
 
     @ManyToOne
-    @JoinColumn
+    @JoinColumn(name = "cinema_id", nullable = false)
     private Cinema cinema;
 
+    @Column(nullable = false)
+    private int seatCount;
+
+    @Enumerated(EnumType.STRING)
+    private ScreenType screenType;
+
+    @Column(nullable = false)
+    private boolean isAvailable;
+
     @Builder
-    public Theater(Long id, String name, Cinema cinema) {
+    public Theater(Long id, String name, Cinema cinema, int seatCount, ScreenType screenType, boolean isAvailable) {
         this.id = id;
         this.name = name;
         this.cinema = cinema;
+        this.seatCount = seatCount;
+        this.screenType = screenType;
+        this.isAvailable = isAvailable;
     }
 }
